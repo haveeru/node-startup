6cd991ffd040246206c2b21a2e6484cb2072ebdc
haveeru <suood_mohamed@hotmail.com>
2019-08-21 12:22:34 -0700

    API handling get request

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res)=> {
-   res.status(200).json({message: 'hello from serve side', app: 'Natorus'});
+// app.get('/', (req, res)=> {
+//    res.status(200).json({message: 'hello from serve side', app: 'Natorus'});
+// });
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res)=> {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length(),
+        data: {
+            // you do not need to write tours: tours (varaibale and value are same)
+            tours
+        }
+    })
 });
 
 const port = 3000;

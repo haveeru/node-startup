2b0b8e43f6ce5965a756dd7473d38389833829cd
haveeru <suood_mohamed@hotmail.com>
2019-08-24 12:56:56 -0700

    Handle delete request

--- app.js
+++ app.js
@@ -54,6 +54,20 @@ app.patch('api/v1/tours/:id', (req, res) => {
     }
 
     res.status(200).json({
+        data: null
+    })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id*1 > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invaildi ID'
+        });
+    }
+
+    res.status(204).json({
+        status: 'success',
         data: {
             tour: '<Updated toru here...>'
         }

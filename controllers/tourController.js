d6e97d322b8787e0b168de1138394dabecf08f52
haveeru <suood_mohamed@hotmail.com>
2019-09-02 11:23:22 +0500

    reading document

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,29 +1,42 @@
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requsetTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requsetTime,
-    // results: tours.length,
-    // data: {
-    //   // you do not need to write tours: tours (varaibale and value are same)
-    //   tours
-    // }
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        // you do not need to write tours: tours (varaibale and value are same)
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.param);
-  const id = req.params.id * 1;
-  // const tour = tours.find(el => el.id === id);
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    // Tour.findOne({ _id: req.params.id})
 
-  res.status(200).json({
-    status: 'success'
-    // data: {
-    //   tour
-    // }
-  });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {

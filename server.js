601975b30e764d97689d693a5ab420ac4ef0b71c
haveeru <suood_mohamed@hotmail.com>
2019-09-02 08:22:31 +0500

    simple tour schemea

--- server.js
+++ server.js
@@ -5,15 +5,35 @@ const app = require('./app');
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>', 
+  '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB, {
-  useNewUrlParser : true,
-  useCreateIndex : true,
-  useFindAndModify : false
-}).then(() => console.log('DB connection successful!'));
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tours = mongoose.model('Tour', tourSchema);
 
 console.log(app.get('env'));
 //console.log(process.env);

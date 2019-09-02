b5eca03534a286457764c1f06993426f7d819aa1
haveeru <suood_mohamed@hotmail.com>
2019-09-02 08:39:28 +0500

    creating and testing document

--- server.js
+++ server.js
@@ -33,7 +33,21 @@ const tourSchema = new mongoose.Schema({
   }
 });
 
-const Tours = mongoose.model('Tour', tourSchema);
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('Error :', err);
+  });
 
 console.log(app.get('env'));
 //console.log(process.env);

c56a47972bab1c5ac6cec9733bf0f093443f6e6b
haveeru <suood_mohamed@hotmail.com>
2019-09-03 13:15:27 +0500

    making api better limiting fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,9 +25,18 @@ exports.getAllTours = async (req, res) => {
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
-      // sor('price ratingsAverage')
+      // sort('price ratingsAverage')
     } else {
       query = query.sort('-createdAt');
+    }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      // excluding '__v' fields
+      query = query.select('-__v');
     }
 
     // const query = await Tour.find()

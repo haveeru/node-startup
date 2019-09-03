4cfc562d2346155fa4c97028c4c7b641fe08f65f
haveeru <suood_mohamed@hotmail.com>
2019-09-03 19:19:50 +0500

    making api better pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,6 +39,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exit');
+    }
+
     // const query = await Tour.find()
     //   .where('duration')
     //   .equals(5)
@@ -46,6 +59,7 @@ exports.getAllTours = async (req, res) => {
     //   .equals('easy');
 
     const tours = await query;
+    // qyery.sort().select().skip().limit()
 
     //SEND RESPONSE
     res.status(200).json({

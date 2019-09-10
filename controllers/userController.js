3b36af989f5d261ab7b090b2d2351e94e131daa2
haveeru <suood_mohamed@hotmail.com>
2019-09-10 13:01:35 +0500

    Loggin in Users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,35 +1,50 @@
+const User = require('./../models/userModel');
+const catchAsync = require('../utils/catchAsync');
 
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'this route is not yet defnied'
-    })
-}
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  // qyery.sort().select().skip().limit()
+
+  //SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      // you do not need to write tours: tours (varaibale and value are same)
+      users
+    }
+  });
+
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not yet defnied'
+  });
+});
 
 exports.getUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'this route is not yet defnied'
-    })
-}
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not yet defnied'
+  });
+};
 
 exports.createUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'this route is not yet defnied'
-    })
-}
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not yet defnied'
+  });
+};
 
 exports.updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'this route is not yet defnied'
-    })
-}
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not yet defnied'
+  });
+};
 
 exports.deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'this route is not yet defnied'
-    })
-}
+  res.status(500).json({
+    status: 'error',
+    message: 'this route is not yet defnied'
+  });
+};
--- models/userModel.js
+++ models/userModel.js
@@ -73,12 +73,12 @@ userSchema.pre('save', async function(next) {
 //   next();
 // });
 
-// userSchema.methods.correctPassword = async function(
-//   candidatePassword,
-//   userPassword
-// ) {
-//   return await bcrypt.compare(candidatePassword, userPassword);
-// };
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 // userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
 //   if (this.passwordChangedAt) {

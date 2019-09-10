68453bad27958a4cd8636479485fbc9942ae410a
haveeru <suood_mohamed@hotmail.com>
2019-09-10 14:19:19 +0500

    protecting routes pt1

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,3 +52,29 @@ exports.login = catchAsync(async (req, res, next) => {
     token
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get acces', 401)
+    );
+  }
+
+  // 2) verification token
+
+  // 3) check if user still exist
+
+  // 4) check if changed password after the JWT was issued
+
+  next();
+});
